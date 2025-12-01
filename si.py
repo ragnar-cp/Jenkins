f8d41180d75da7160ae15453f694099caf566992
Chetan Patil <patilvchetan@gmail.com>
2025-12-01 11:48:34 +0530

    Refactor input to use command line arguments
    Updated input method to accept command line arguments.

--- si.py
+++ si.py
@@ -1,6 +1,12 @@
-principal = float(input("Enter principal amount: "))
-rate = float(input("Enter rate of interest: "))
-time = float(input("Enter time in years: "))
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: python3 si.py <principal> <rate> <time>")
+    sys.exit(1)
+
+principal = float(sys.argv[1])
+rate = float(sys.argv[2])
+time = float(sys.argv[3])
 
 si = (principal * rate * time) / 100
 print("Simple Interest:", si)
