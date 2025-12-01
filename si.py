2d688fb8fbab3076cbdba2aef173f333a810d62a
Chetan Patil <patilvchetan@gmail.com>
2025-12-01 11:46:32 +0530

    Fix variable name for simple interest calculation

--- si.py
+++ si.py
@@ -1,8 +1,6 @@
-# Simple Interest = (Principal × Rate × Time) / 100
-
 principal = float(input("Enter principal amount: "))
 rate = float(input("Enter rate of interest: "))
 time = float(input("Enter time in years: "))
 
-simple_interest = (principal * rate * time) / 100
-print("Simple Interest:", simple_interest)
+si = (principal * rate * time) / 100
+print("Simple Interest:", si)
